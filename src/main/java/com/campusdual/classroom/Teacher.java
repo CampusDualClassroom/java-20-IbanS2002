73a0bec86beb1aa627cb12c99733227b47248854
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-09-27 14:06:48 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Teacher.java
+++ src/main/java/com/campusdual/classroom/Teacher.java
@@ -8,4 +8,9 @@ public class Teacher extends Person {
 		super(name, surname);
 		this.area = area;
 	}
+
+	@Override
+	public void getDetails() {
+		System.out.println("Teacher: " + name + " " + surname + ", Area: " + area);
+	}
 }
